d08d1161bc10780d10de1ff846c7f5199e267abf
Phung Nguyen <phung.nguyen@beesightsoft.com>
2018-08-21 17:51:25 +0700

    update Calendar Screen

--- lib/Home.Screen.dart
+++ lib/Home.Screen.dart
@@ -1,116 +1,6 @@
 import 'package:flutter_open/CustomCalendar.dart';
-import 'package:date_utils/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
-/*class InputDropdown extends StatelessWidget {
-  const InputDropdown(
-      {Key key,
-        this.child,
-        this.labelText,
-        this.valueText,
-        this.valueStyle,
-        this.onPressed})
-      : super(key: key);
-
-  final String labelText;
-  final String valueText;
-  final TextStyle valueStyle;
-  final VoidCallback onPressed;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return new InkWell(
-      onTap: onPressed,
-      child: new InputDecorator(
-        decoration:
-        new InputDecoration(labelText: labelText, border: InputBorder.none),
-        baseStyle: valueStyle,
-        child: new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            new Text(valueText, style: valueStyle),
-            new Icon(Icons.arrow_drop_down,
-                color: Theme.of(context).brightness == Brightness.light
-                    ? Colors.grey.shade700
-                    : Colors.white70),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DatePickerWithDuration extends StatelessWidget {
-  const DatePickerWithDuration({
-    Key key,
-    this.labelText,
-    this.selectedDate,
-    this.selectDate,
-    this.remove,
-  }) : super(key: key);
-
-  final String labelText;
-  final DateTime selectedDate;
-  final ValueChanged<DateTime> selectDate;
-  final remove;
-
-  Future<Null> _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: new DateTime(2015, 8),
-        lastDate: new DateTime(2101));
-    if (picked != null && picked != selectedDate) selectDate(picked);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle valueStyle = Theme.of(context).textTheme.subhead;
-    return new Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new Expanded(
-          flex: 3,
-          child: new InputDropdown(
-            labelText: labelText,
-            valueText: new DateFormat.yMMMd().format(selectedDate),
-            valueStyle: valueStyle,
-            onPressed: () {
-              _selectDate(context);
-            },
-          ),
-        ),
-        const SizedBox(width: 12.0),
-        new Expanded(
-          flex: 4,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('AM'),
-                Checkbox(
-                  value: true,
-                  onChanged: null,
-                ),
-                Text('PM'),
-                Checkbox(
-                  value: true,
-                  onChanged: null,
-                ),
-                IconButton(icon: Icon(Icons.remove_circle_outline), onPressed: remove),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}*/
 
 class PersonalDialog extends StatefulWidget {
   PersonalDialog();
@@ -211,6 +101,7 @@ class PersonalDialogState extends State<PersonalDialog> {
                         child: CustomCalendar(
                             selectedPositions: selectedPositions,
                             onSelectDay: (SelectedDateInfo info) {
+                              FocusScope.of(context).requestFocus(new FocusNode());
                               if (info.session != null && info.day != null) {
                                 print(info.session.toString() +
                                     ' - ' +
